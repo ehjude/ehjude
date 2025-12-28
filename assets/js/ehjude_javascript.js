c73ee21591ff10cfd59cad00a30a52505f65b55b
ehjude <jude.fiorillo@gmail.com>
2025-12-28 13:24:04 -0500

    new ehjude.com portfolio site

--- assets/js/ehjude_javascript.js
+++ assets/js/ehjude_javascript.js
@@ -1,4 +1,183 @@
 $(document).on("ready page:load", function (){
+    // Check if we're on mobile/tablet
+    function isMobile() {
+        return window.innerWidth <= 1050;
+    }
+    
+    // EXPERIENCE - Modified for responsive behavior
+    $("#experience-container" ).on( "mouseenter", ".job-target-outer-container", function( event ) {
+        // Skip hover effects on mobile/tablet
+        if (isMobile()) {
+            return;
+        }
+        
+        // Desktop hover effects
+        $('.job-outer-container', this).css({'background-color': '#0065a3'});
+     	$('.job-inner-container', this).css({'color': '#fff'});
+     	$('.job-inner-container', this).css({'border': '#fff'});
+     	$('.job-company', this).css({'color': '#fff'});
+     	$('.experience-arrow', this).attr('src','assets/images/arrow_white.png');
+		$('.job-inner-container', this).css({'padding-left': '20px'});
+    }).on( "mouseleave", ".job-target-container", function( event ) {
+        // Skip hover effects on mobile/tablet
+        if (isMobile()) {
+            return;
+        }
+        
+        // Desktop hover reset effects
+        $('.job-outer-container', this).css({'background-color': '#fff'});
+        $('.job-inner-container', this).css({'color': '#181819'});
+     	$('.job-inner-container', this).css({'border-top': '1px solid #ccd0d2'});
+     	$('.job-company', this).css({'color': '#0065a3'});
+     	$('.experience-arrow', this).attr('src','assets/images/arrow.png');
+		$('.job-inner-container', this).css({'padding-left': '0'});
+    });
+
+	// Modified click handler for responsive accordion
+	$("#experience" ).on( "click", ".job-target-container", function( event ) {
+		event.preventDefault();
+		
+		if (isMobile()) {
+			// Mobile accordion behavior - simplified
+			var isCurrentlyOpen = $(this).hasClass('mobile-open');
+			var clickedJobContainer = $(this);
+			
+			$('.job-target-container').removeClass('mobile-open');
+			$(".job-details-container").slideUp(300);
+			
+			if (!isCurrentlyOpen) {
+				clickedJobContainer.addClass('mobile-open');
+				clickedJobContainer.next('.job-details-container').slideDown(300);
+				
+				setTimeout(function() {
+					$([document.documentElement, document.body]).animate({
+						// scrollTop: clickedJobContainer.offset().top - 70
+						scrollTop: clickedJobContainer.offset().top - 10
+					}, 500);
+				}, 350);
+			}
+		} else {
+			// Desktop accordion behavior - improved logic
+			var currentMaxWidth = $('.job-target-outer-container', this).css('max-width');
+			var isCurrentlyOpen = false;
+			
+			if (currentMaxWidth === 'none') {
+				isCurrentlyOpen = true;
+			} else {
+				var maxWidthValue = parseInt(currentMaxWidth);
+				if (!isNaN(maxWidthValue) && maxWidthValue > 1050) {
+					isCurrentlyOpen = true;
+				} else if (currentMaxWidth === '100%' || currentMaxWidth.includes('%')) {
+					isCurrentlyOpen = true;
+				}
+			}
+			
+			var detailsVisible = $(this).next('.job-details-container').is(':visible');
+			
+			if (isCurrentlyOpen || detailsVisible) {
+				// Close this accordion
+				$(".job-details-container").hide();
+				$('.job-target-outer-container', this).css({'max-width': '1050px'});
+				$('.job-outer-container', this).css({
+					'padding-left': '25px',
+					'border-top': '0px',
+					'border-radius': '5px'
+				});
+				$('.job-inner-container', this).css({'width': '899px'}); // Restore original width
+			} else { 
+				// Close all other accordions first 
+				$('.job-target-outer-container').not(this).css({'max-width': '1050px'});
+				$('.job-outer-container').not($(this).find('.job-outer-container')).css({
+					'padding-left': '25px',
+					'border-top': '0px',
+					'border-radius': '5px'
+				});
+				$('.job-inner-container').not($(this).find('.job-inner-container')).css({'width': '899px'}); // Restore original width, not 'initial'
+				$(".job-details-container").hide();
+
+				// Open this accordion with responsive widths
+				var windowWidth = $(window).width();
+				var containerWidth = windowWidth > 1200 ? '100%' : '95%';
+				var innerWidth = windowWidth > 1200 ? '1150px' : 'calc(100% - 105px)';
+				var paddingLeft = windowWidth > 1200 ? '175px' : '20px';
+				
+				// $(this).find('.job-inner-container').css({'border-top': 'none !important'});
+
+				$('.job-target-outer-container', this).css({'max-width': containerWidth});
+				$('.job-outer-container', this).css({
+					'padding-left': paddingLeft,
+					'border-top': '1px solid #ccd0d2',
+					'border-radius': '0'
+				});
+				$('.job-inner-container', this).css({'width': innerWidth});
+
+				$(this).next('.job-details-container').show();
+
+				$([document.documentElement, document.body]).animate({
+					scrollTop: $(".job-outer-container", this).offset().top - 60
+				}, 700);
+
+				// When hovering over job title bar, change background to blue
+				$("#experience-container" ).on( "mouseenter", ".job-target-outer-container", function( event ) {
+					$('.job-outer-container', this).css({'background-color': '#0065a3'});
+				}).on( "mouseleave", ".job-target-container", function( event ) {
+					$('.job-outer-container', this).css({'background-color': '#fff'});
+				});
+			}
+		}
+	});
+
+	// Also update your resize handler:
+	$(window).on('resize', function() {
+		$('.job-target-container').removeClass('mobile-open');
+		$('.job-target-outer-container').css({'max-width': '1050px'});
+		$('.job-outer-container').css({
+			'padding-left': '25px',
+			'background-color': '#fff',
+			'border-top': '0px',
+			'border-radius': '5px'
+		});
+		
+		if (isMobile()) {
+			$('.job-inner-container').css({
+				'border-top': 'none',
+				'color': '#181819'
+			});
+		} else {
+			$('.job-inner-container').css({
+				'width': '899px', // Use actual CSS width instead of 'initial'
+				'border-top': '1px solid #ccd0d2',
+				'color': '#181819'
+			});
+		}
+
+		$('.job-company').css({
+			'color': '#0065a3',
+			'visibility': 'initial'
+		});
+		$('.job-date-arrow').css({'right': '0'});
+		$('.experience-arrow').attr('src','assets/images/arrow.png');
+		$('.job-title-company').css({'width': '664px'});
+		$('.job-icon').css('top','-10px');
+		$('.job-icon-container').css({
+			'height':'66px',
+			'width': 'initial'
+		});
+		$('.job-title').css({
+			'top': '0',
+			'font-size': '26px'
+		});
+		
+		$(".job-details-container").hide();
+	});
+
+
+
+
+
+
+
+	// ????
 	$(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -14,12 +193,29 @@ $(document).on("ready page:load", function (){
 	  });
 	});
 
+
 	// MODAL
 	$(function() {
+		var scrollPosition = 0;
+		
 		$('.easy-modal').easyModal({
 			top: 60,
 			overlay: 0.2,
-			overlayOpacity: 0.7
+			overlayOpacity: 0.7,
+			onOpen: function(myModal) {
+				// Store current scroll position
+				scrollPosition = $(window).scrollTop();
+				
+				// Disable body scrolling when modal opens
+				$('body').addClass('modal-open').css('top', -scrollPosition + 'px');
+			},
+			onClose: function(myModal) {
+				// Re-enable body scrolling when modal closes
+				$('body').removeClass('modal-open').css('top', '');
+				
+				// Restore scroll position
+				$(window).scrollTop(scrollPosition);
+			}
 		});
 
 		$('.easy-modal-open').click(function(e) {
@@ -37,16 +233,37 @@ $(document).on("ready page:load", function (){
 			overlay: 0.2,
 			transitionIn: 'animated bounceInLeft',
 			transitionOut: 'animated bounceOutRight',
-			closeButtonClass: '.animated-close'
+			closeButtonClass: '.animated-close',
+			onOpen: function(myModal) {
+				// Store current scroll position
+				scrollPosition = $(window).scrollTop();
+				
+				// Disable body scrolling when modal opens
+				$('body').addClass('modal-open').css('top', -scrollPosition + 'px');
+			},
+			onClose: function(myModal) {
+				// Re-enable body scrolling when modal closes
+				$('body').removeClass('modal-open').css('top', '');
+				
+				// Restore scroll position
+				$(window).scrollTop(scrollPosition);
+			}
 		});
 	});
+
+	
+
+
+
+
+
 
 
 	// WAYPOINTS
 	$(function() {
 
 		// Do our DOM lookups beforehand
-		var nav_container = $(".nav-container");
+		var nav_container = $("#nav-container");
 		var nav = $("nav");
 		
 		var top_spacing = 0;
@@ -104,13 +321,5 @@ $(document).on("ready page:load", function (){
 		});
 	});
 
-
-	// Slideshow
-	jQuery('#camera_wrap_2').camera({
-		height: '600px',
-		loader: 'bar',
-		pagination: false,
-		thumbnails: true
-	});
 
 });
